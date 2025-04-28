8760cf31c41595992c2bebe722b5f9e76c414f9d
gLitwin <gabriellitwin@assertsolutions.com>
2025-04-28 12:33:57 -0300

    nueva clase Alojamiento

---
+++ entities/Alojamiento.js
@@ -0,0 +1,56 @@
+export class Alojamiento {
+  constructor(
+    anfitrion,
+    nombre,
+    descripcion,
+    precioPorNoche,
+    moneda,
+    horarioCheckIn,
+    horarioCheckOut,
+    direccion,
+    cantHuespedesMax
+    //TODO hace falta inicializar reservas, fotos y caracteristicas?....
+  ) {
+    this.anfitrion = anfitrion;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precioPorNoche = precioPorNoche;
+    this.moneda = moneda;
+    this.horarioCheckIn = horarioCheckIn;
+    this.horarioCheckOut = horarioCheckOut;
+    this.direccion = direccion;
+    this.cantHuespedesMax = cantHuespedesMax;
+    this.caracteristicas = [];
+    this.reservas = [];
+    this.fotos = [];
+  }
+
+  estaDisponibleEn(rangoDeFechas) {
+    const reserva = this.reservas.find((reserva) =>
+      reserva.rangoDeFechas.seSuperponeCon(rangoDeFechas)
+    );
+    return Boolean(reserva);
+    /*
+      return reserva === undefined
+      return !!reserva 
+    */
+  }
+
+  suPrecioEstaDentroDe(valorMinimo, valorMaximo) {
+    return (
+      this.precioPorNoche >= valorMinimo && this.precioPorNoche <= valorMaximo
+    );
+  }
+
+  tieneCaracteristica(caracteristica) {
+    return this.caracteristicas.includes(caracteristica);
+  }
+
+  puedenAlojarse(cantHuespedes) {
+    return cantHuespedes <= this.cantHuespedesMax;
+  }
+
+  agregarReserva(reserva) {
+    this.reservas.push(reserva);
+  }
+}
