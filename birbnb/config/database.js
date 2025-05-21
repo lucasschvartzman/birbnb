da34d0bf014e1e4bc97d5c0f6c208d0e9d16f22a
Lucas Schvartzman <lucasnicolasschvartzman@gmail.com>
2025-05-21 18:43:45 -0300

    Agrego conexión con MongoDB

--- birbnb/config/database.js
+++ birbnb/config/database.js
@@ -2,6 +2,13 @@ import mongoose from 'mongoose';
 
 export class ClienteMongoDB {
     static async conectar() {
-        // TODO: Hacer
+        try {
+            const uri = process.env.MONGODB_URI + "/" + process.env.MONGODB_DB_NAME;
+            const connection = await mongoose.connect(uri);
+            console.log(`MongoDB está corriendo en: ${connection.connection.host}`);
+        } catch (error) {
+            console.error(`Error: ${error.message}`);
+            process.exit(1);
+        }
     }
 }
