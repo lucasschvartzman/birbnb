ec2193f1065fcf8f42416cbe7fe474c791c37c02
Lucas Schvartzman <lucasnicolasschvartzman@gmail.com>
2025-05-22 18:28:05 -0300

    Agrego persistencia de notificación

--- birbnb/models/repositories/notificacionRepository.js
+++ birbnb/models/repositories/notificacionRepository.js
@@ -2,9 +2,30 @@ import {NotificacionModel} from "../schemas/notificacionSchema.js";
 
 export class NotificacionRepository {
 
-  constructor() {
-    this.model = NotificacionModel;
-  }
+    constructor() {
+        this.model = NotificacionModel;
+    }
 
-  // TODO: Hacer los métodos de persistencia
+    async save(notificacion) {
+        const query = notificacion.id
+            ? {_id: notificacion.id}
+            : {_id: new this.model()._id};
+
+        return this.model.findOneAndUpdate(
+            query,
+            notificacion,
+            {new: true, upsert: true}
+        ).populate('usuario');
+    }
+
+    async findAll(filters = {}) {
+        const query = {};
+        if (filters.idUsuario) {
+            query.usuario = filters.idUsuario;
+        }
+        if (filters.leida !== undefined) {
+            query.leida = filters.leida;
+        }
+        return this.model.find(query).populate('usuario');
+    }
 }
