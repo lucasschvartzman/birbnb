9582154511f94bfded49624f5ba348c038b4adcd
jessica.saavedra <jessica.saavedra@withstensul.com>
2025-06-14 20:31:01 -0300

    Status en appExceptions.js

--- birbnb/exceptions/appExceptions.js
+++ birbnb/exceptions/appExceptions.js
@@ -2,6 +2,7 @@ export class AppException extends Error {
     constructor(message, statusCode) {
         super(message);
         this.statusCode = statusCode;
+        // Por qué este status? no entiendo qué les aporta, se podría boletear
         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
         this.esOperacional = true;
         Error.captureStackTrace(this, this.constructor);
