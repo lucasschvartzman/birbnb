eea2d936d7ffcc26d6dcd287095beb273c18026b
Lucas Schvartzman <lschvartzman@frba.utn.edu.ar>
2025-06-17 14:21:50 -0300

    Elimino atributo "status" en AppException sin utilidad.

--- birbnb/middlewares/exceptionHandler.js
+++ birbnb/middlewares/exceptionHandler.js
@@ -1,10 +1,7 @@
 export const exceptionHandler = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-
     if (process.env.NODE_ENV === 'dev') {
         res.status(err.statusCode).json({
-            status: err.status,
             error: err,
             message: err.message,
             stack: err.stack
@@ -12,13 +9,11 @@ export const exceptionHandler = (err, req, res, next) => {
     } else {
         if (err.esOperacional) {
             res.status(err.statusCode).json({
-                status: err.status,
                 message: err.message
             });
         } else {
             console.error('[ERROR]: ', err);
             res.status(500).json({
-                status: 'error',
                 message: 'Ocurrió un error inesperado.'
             });
         }
--- birbnb/middlewares/notFoundHandler.js
+++ birbnb/middlewares/notFoundHandler.js
@@ -1,6 +1,5 @@
 export const notFoundHandler = (req, res, next) => {
     res.status(404).json({
-        status: 'fail',
         message: "La ruta solicitada no existe."
     });
 };
