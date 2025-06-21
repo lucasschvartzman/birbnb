fcbe5bf5fc4b2da9516b65f9a836eacf5aaf4a4a
gLitwin <gabriellitwin@assertsolutions.com>
2025-06-21 02:44:07 -0300

    ADD Powershell script bien basico para levantar front y back al mismo tiempo

---
+++ run.ps1
@@ -0,0 +1,21 @@
+$thisPath = Get-Location
+$backendPath = "$thisPath\backend"
+$frontendPath = "$thisPath\frontend"
+$installDepsFrontendCommand = "npm i"
+$installDepsBackendCommand = "npm i"
+$startBackendCommand = "npm run start"#"node index.js --watch"
+$startFrontendCommand = "npm run start"#"react-scripts start"
+
+try {
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd '$backendPath'; $installDepsBackendCommand; $startBackendCommand" -PassThru
+Start-Sleep -Seconds 1
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd '$frontendPath'; $installDepsFrontendCommand; $startFrontendCommand" -PassThru
+    
+Write-Host ""
+Write-Host "¡Ambos proyectos iniciados exitosamente!" -ForegroundColor Green
+
+}
+finally {
+    Set-Location "$thisPath"
+}
+
