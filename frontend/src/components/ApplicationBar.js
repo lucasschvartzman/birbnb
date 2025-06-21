d7b9cd73b11d06c61f741aa79c54edec186429d1
JURADO SURCO Romina B. TENARIS <RBJURADOSURCO@tenaris.com>
2025-06-21 15:44:23 -0300

    Agrego icono de perfil con inicial, modifico el contexto para guardar la inicial

--- frontend/src/components/ApplicationBar.js
+++ frontend/src/components/ApplicationBar.js
@@ -1,23 +1,37 @@
 import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
-import {AppBar, Button, Toolbar, Typography} from '@mui/material';
-import {useAuth} from "../context/AuthContext";
-import {useNavigate} from "react-router";
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router";
 import '../features/navbar/navbar.css'; // 
-import {IconButton, Badge, Drawer, List, ListItem, ListItemText} from '@mui/material';
+import { IconButton, Badge, Drawer, List, ListItem, ListItemText, Avatar, Menu, MenuItem } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import LogoutIcon from '@mui/icons-material/Logout';
-import logo from '../assets/logo.png'; 
+import logo from '../assets/logo.png';
 
 const ApplicationBar = () => {
-  const {estaAutenticado, clearAuthContext} = useAuth();
-  const navigate = useNavigate();
+  const { estaAutenticado, clearAuthContext } = useAuth();
+  
+  //para la inicial
+  const { email } = useAuth();
+  const inicial = email?.charAt(0).toUpperCase() || '?';
 
+
+  // Estado para el menu de logo icono
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+
+  const navigate = useNavigate();
   const handleLoginClick = () => {
     navigate('/login');
   }
-
   const handleLogout = () => {
     clearAuthContext();
     navigate('/');
@@ -27,85 +41,96 @@ const ApplicationBar = () => {
   const toggleDrawer = (open) => () => setDrawerOpen(open);
 
   const drawerList = (
-  <List className="drawer-list">
-    <ListItem button component={Link} to="/" onClick={toggleDrawer(false)}>
-      <ListItemText primary="Home"   primaryTypographyProps={{ style: { color: 'black' } }} />
-    </ListItem>
-    <ListItem button component={Link} to="/login" onClick={toggleDrawer(false)}>
-      <ListItemText primary="Ingresar"   primaryTypographyProps={{ style: { color: 'black' } }} />
-    </ListItem>
-
-    {estaAutenticado && (
-      <>
-      <ListItem button component={Link} to="/reservas" onClick={toggleDrawer(false)}>
-        <ListItemText primary="Reservas" primaryTypographyProps={{ style: { color: 'black' } }}/>
+    <List className="drawer-list">
+      <ListItem button component={Link} to="/" onClick={toggleDrawer(false)}>
+        <ListItemText primary="Home" primaryTypographyProps={{ style: { color: 'black' } }} />
       </ListItem>
-      <ListItem button onClick={handleLogout}>
-          <ListItemText primary="Cerrar Sesión"></ListItemText>
-     </ListItem>
-     </>
-    )}
-  </List>
+
+      {!estaAutenticado && (
+        <ListItem button component={Link} to="/login" onClick={toggleDrawer(false)}>
+          <ListItemText primary="Ingresar" primaryTypographyProps={{ style: { color: 'black' } }} />
+        </ListItem>
+      )}
+
+      {estaAutenticado && (
+        <>
+          <ListItem button component={Link} to="/reservas" onClick={toggleDrawer(false)}>
+            <ListItemText primary="Reservas" primaryTypographyProps={{ style: { color: 'black' } }} />
+          </ListItem>
+        </>
+      )}
+    </List>
   );
 
 
   return (
-<AppBar position="static" className="navbar">
-  <Toolbar className="toolbar">
+    <AppBar position="static" className="navbar">
+      <Toolbar className="toolbar">
 
-    {/* IZQUIERDA: Logo */}
-    <div className="logo-section">
-      <Link to="/">
-        <img src="images/logo-light.png" alt="Birbnb" className="logo-icon" />
-      </Link>
+        {/* IZQUIERDA: Logo */}
+        <div className="logo-section">
+          <Link to="/">
+            <img src="images/logo-light.png" alt="Birbnb" className="logo-icon" />
+          </Link>
 
-      <div className="nav-left">
-       {estaAutenticado && (<>
-      <Button component={Link} to="/reservas" className="nav-button">Reservas</Button>
-      </>)} 
-      </div>
-    </div>
+          <div className="nav-left">
+            {estaAutenticado && (<>
+              <Button component={Link} to="/reservas" className="nav-button">Reservas</Button>
+            </>)}
+          </div>
+        </div>
 
-    {/* DERECHA: Ingresar */}
-    <div className="auth-button">
-    {estaAutenticado ? (
-     <>
-     <IconButton className="notif-button" >
-      <Badge badgeContent={3} color="error">
-        <NotificationsIcon sx={{ color: '#ffffff' }} />
-      </Badge>
-      </IconButton>
-     <div className='nav-right '>
-      <Button color="inherit" className="nav-button" onClick={handleLogout}>
-       <LogoutIcon sx={{ color: '#FFFFF' }}/>
-      </Button>
-      </div>
-     
-     </>
-    ) : (
-     <div className='nav-right'>
-      <Button color="inherit" className="nav-button" onClick={handleLoginClick}>Ingresar</Button>
-     </div>
-    )}
+        {/* DERECHA: Ingresar */}
+        <div className="auth-button">
+          {estaAutenticado ? (
+            <>
+              <IconButton className="notif-button">
+                <Badge badgeContent={3} color="error">
+                  <NotificationsIcon sx={{ color: '#ffffff' }} />
+                </Badge>
+              </IconButton>
 
-    </div>
+              {/* Avatar con menú */}
+              <IconButton onClick={handleMenuOpen}>
+                <Avatar sx={{ bgcolor: '#f48fb1', width: 30, height: 30 }}>{inicial}</Avatar>
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                disableScrollLock={true}
+                onClose={handleMenuClose}>
+
+                <MenuItem onClick={() => { handleMenuClose(); handleLogout(); }}>
+                  <LogoutIcon fontSize="small" sx={{ mr: 1 }} />
+                  Cerrar sesión
+                </MenuItem>
+              </Menu>
+            </>
+          ) : (
+            <div className='nav-right'>
+              <Button color="inherit" className="nav-button" onClick={handleLoginClick}>Ingresar</Button>
+            </div>
+          )}
 
 
-    {/* Drawer para mobile */}
-    <IconButton
-       edge="end"
-       className="menu-button"
-       onClick={toggleDrawer(true)}
-       sx={{ display: { xs: 'block', md: 'none' } }} 
-    >
-      <MenuIcon sx={{ color: '#ffffff' }} />
-    </IconButton>
-  </Toolbar>
+        </div>
 
-  <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
-    {drawerList}
-  </Drawer>
-</AppBar>
+
+        {/* Drawer para mobile */}
+        <IconButton
+          edge="end"
+          className="menu-button"
+          onClick={toggleDrawer(true)}
+          sx={{ display: { xs: 'block', md: 'none' } }}
+        >
+          <MenuIcon sx={{ color: '#ffffff' }} />
+        </IconButton>
+      </Toolbar>
+
+      <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
+        {drawerList}
+      </Drawer>
+    </AppBar>
 
   );
 }
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,22 +1,27 @@
-import {createContext, useContext, useState} from "react";
+import { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [idUsuario, setIdUsuario] = useState(null);
+  const [email, setEmail] = useState(null); // nuevo estado
 
-  const setAuthContext = (idUsuario) => {
-    setIdUsuario(idUsuario);
-  }
+  const setAuthContext = (id, emailUsuario) => {
+    setIdUsuario(id);
+    setEmail(emailUsuario); // guardo el email para la inicial
+  };
 
   const clearAuthContext = () => {
     setIdUsuario(null);
-  }
+    setEmail(null);
+  };
 
   const estaAutenticado = idUsuario !== null;
 
   return (
-    <AuthContext.Provider value={{ idUsuario, setAuthContext, clearAuthContext, estaAutenticado}}>
+    <AuthContext.Provider
+      value={{ idUsuario, email, setAuthContext, clearAuthContext, estaAutenticado }}
+    >
       {children}
     </AuthContext.Provider>
   );
