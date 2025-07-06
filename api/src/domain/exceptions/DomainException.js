322ab546b0490a018562fe488ecda5f366cbbd01
Lucas Schvartzman <lschvartzman@frba.utn.edu.ar>
2025-07-06 19:14:49 -0300

    refactor: define architecture using DDD
    First approach, more things will be added later.

---
+++ api/src/domain/exceptions/DomainException.js
@@ -0,0 +1,8 @@
+export class DomainException extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperation = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
