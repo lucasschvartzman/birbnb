b92d72bd61f2162a98776f68f92a0c0f2808c19c
Lucas Schvartzman <lschvartzman@frba.utn.edu.ar>
2025-07-07 14:48:43 -0300

    chore: update JSDocs on City and Country repositories

--- api/src/domain/repositories/ICityRepository.js
+++ api/src/domain/repositories/ICityRepository.js
@@ -4,15 +4,19 @@ import {NotImplementedException} from "../exceptions/NotImplementedException";
  * Repository interface for City entities.
  *
  * Defines the contract that all implementations must follow.
+ *
+ * @note All methods operate on domain entities and use UUIDv4 strings as identifiers.
+ *
  * @abstract
  * @class
  */
 export class ICityRepository {
 
   /**
-   * Retrieves all cities from the data source.
-   * @param {number} id Country identifier.
-   * @returns {Promise<Country[]>} Array of cities from specified country.
+   * Finds all Cities for a given country.
+   *
+   * @param {string} id - UUIDv4 identifier of the country.
+   * @returns {Promise<City[]>} An array of Cities (empty if none found).
    * @throws {NotImplementedException} When called without implementation.
    */
   async findAllByCountry(id) {
--- api/src/domain/repositories/ICountryRepository.js
+++ api/src/domain/repositories/ICountryRepository.js
@@ -4,14 +4,18 @@ import {NotImplementedException} from "../exceptions/NotImplementedException";
  * Repository interface for Country entities.
  *
  * Defines the contract that all implementations must follow.
+ *
+ * @note All methods operate on domain entities and use UUIDv4 strings as identifiers.
+ *
  * @abstract
  * @class
  */
 export class ICountryRepository {
 
   /**
-   * Retrieves all countries from the data source.
-   * @returns {Promise<Country[]>} Array of countries.
+   * Finds all Countries
+   *
+   * @returns {Promise<Country[]>} An array of Countries (empty if none found).
    * @throws {NotImplementedException} When called without implementation.
    */
   async findAll() {
